17e69655bd40a204b64b566fbc32ed66e84c0f45
Danny Nadarajah <Danny_Nadarajah@hotmail.com>
2020-01-07 19:37:29 -0500

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 
 
 const port = process.env.PORT || 3000;
@@ -8,27 +9,13 @@ app.listen(port, () => console.log('Listening for connections'));
 
 
 app.use(express.static(__dirname));
+app.use(express.json({ limit: '10mb' }));
 
-
-const url = 'http://ddragon.leagueoflegends.com/cdn/9.24.2/data/en_US/champion.json';
 let championNameArrayOnly = [];
-(async function fetchData() {
+const url = 'http://ddragon.leagueoflegends.com/cdn/9.24.2/data/en_US/champion.json';
+
+app.get('/api', async (req, res) => {
     const response = await fetch(url);
     const jsonObject = await response.json();
-    championNameArrayOnly = Object.keys(jsonObject.data);
-})();
-
-let championList = {};
-(async function fetchData() {
-    const response = await fetch(url);
-    const jsonObject = await response.json();
-    championList = jsonObject.data;
-
-})();
-
-
-
-
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.send(jsonObject.data);
 })
