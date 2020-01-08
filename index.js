2eb8643c820681ca9087980e31d20114dcad2b3d
Danny Nadarajah <Danny_Nadarajah@hotmail.com>
2020-01-07 19:09:27 -0500

    make it better

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ app.listen(port, () => console.log('Listening for connections'));
 app.use(express.static(__dirname));
 
 
+const url = 'http://ddragon.leagueoflegends.com/cdn/9.24.2/data/en_US/champion.json';
+let championNameArrayOnly = [];
+(async function fetchData() {
+    const response = await fetch(url);
+    const jsonObject = await response.json();
+    championNameArrayOnly = Object.keys(jsonObject.data);
+})();
+
+let championList = {};
+(async function fetchData() {
+    const response = await fetch(url);
+    const jsonObject = await response.json();
+    championList = jsonObject.data;
+
+})();
+
+
+
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
